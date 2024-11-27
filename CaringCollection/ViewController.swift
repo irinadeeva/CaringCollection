78a78ae2f8415bd07edaf39b8d206b421182ab9b
irinadeeva <deeva.irina.a@gmail.com>
2024-11-27 08:27:13 +0300

    Initial Commit

---
+++ CaringCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaringCollection
+//
+//  Created by Irina Deeva on 27/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
